6f2d2ecfd50c64c80017dfad8fe50c3bf76f7f2d
minyi.yang <minyi.yang2019@gmail.com>
2020-07-30 20:55:38 +0900

    JS: Transparent

--- main.js
+++ main.js
@@ -26,9 +26,16 @@ navbarMenu.addEventListener("click", (event) => {
 });
 
 // Handle click on "contact me" button on home
-const homeContactBtn = document.querySelector(".home__contact");
+const homeContactBtn = document.querySelector(".home__container");
 homeContactBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 function scrollIntoView(selector) {
